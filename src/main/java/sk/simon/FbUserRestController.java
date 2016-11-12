e7b96ad8abea8e103ce85fb5624d0ac90d5e8685
Simon Sudora <simon.sudora@gmail.com>
2016-11-12 14:06:49 +0100

    add delete endpoint

--- src/main/java/sk/simon/FbUserRestController.java
+++ src/main/java/sk/simon/FbUserRestController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sk.simon.exceptions.FbUserNotDeleted;
 import sk.simon.exceptions.FbUserNotRetrieved;
 import sk.simon.exceptions.FbUserNotSaved;
 
@@ -16,19 +14,19 @@ import sk.simon.exceptions.FbUserNotSaved;
  */
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class FbUserRestController {
 
     @Autowired
     private FbUserService fbUserService;
 
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity retrieveAndSaveFbUser(@RequestBody FbUserAccessDetailsDTO fbUserAccessDetails){
         try {
             fbUserService.retrieveAndSaveFbUser(fbUserAccessDetails.getAccessToken(),fbUserAccessDetails.getUserId());
             return ResponseEntity
                     .status(HttpStatus.CREATED)
-                    .body("User details were successfully tetrieved form Fb and store in database ");
+                    .body("User details were successfully retrieved from Fb and store in database ");
         } catch (FbUserNotSaved fbUserNotSaved) { //vyhodit erroro cody
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -39,12 +37,21 @@ public class FbUserRestController {
                     .body("User details was not retrieved from Facebook");        }
     }
 
-    @RequestMapping(method = RequestMethod.DELETE)
-    public void deleteUserAndHisPages(){
-
+    @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteUserAndHisPages(@PathVariable String userId){
+        try {
+            fbUserService.deleteFbUser(userId);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("User was successfully deleted from database");
+        } catch (FbUserNotDeleted fbUserNotDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("User was not deleted from database");
+        }
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public void getFbUserDetails(){
 
     }
--- src/test/java/sk/simon/FbUserControllerTest.java
+++ src/test/java/sk/simon/FbUserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +30,9 @@ public class FbUserControllerTest {
     //Token expires after few hours
     private String accessToken = "EAAQx8CaKKZCoBAIY0UkTThZABVu3unZA78w8zACOyf5ZCxty5xZCyEqMURoSJKpZA4VH1QyhlMxhsUbGY3sjtimenibyMl29MBZC3TDyWZCLo9hkJslG0sVJT2aPCQnTTdmr2uAuQBflOgAB5CZAMW2EUXZBw6GvZAO6qHou7pwi4wrAAZDZD";
     private String userFbId = "10206248904662029";
+
+    @Autowired
+    private FbUserRepository fbUserRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -48,4 +52,49 @@ public class FbUserControllerTest {
                 .content(objectMapper.writeValueAsBytes(fbUserAccessDetailsDTO)))
                 .andExpect(status().isCreated());
     }
+
+    @Test
+    public void retrieveAndSaveFbUserWrongIdTest() throws Exception {
+        String wrongUserFbId = "029";
+
+        FbUserAccessDetailsDTO fbUserAccessDetailsDTO = new FbUserAccessDetailsDTO();
+        fbUserAccessDetailsDTO.setAccessToken(accessToken);
+        fbUserAccessDetailsDTO.setUserId(wrongUserFbId);
+
+        objectMapper.writeValueAsBytes(fbUserAccessDetailsDTO);
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(fbUserAccessDetailsDTO)))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    public void deleteUserAndHisPagesTest() throws Exception {
+        FbUserEntity fbUser = new FbUserEntity();
+        fbUser.setLastName("Sudora");
+        fbUser.setFirstName("Simon");
+        fbUser.setGender("male");
+        fbUser.setFbId(userFbId);
+        fbUserRepository.save(fbUser);
+
+        mvc.perform(delete("/users/" + userFbId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteUserAndHisPagesWrongIdTest() throws Exception {
+        String wrongUserFbId = "029";
+
+        FbUserEntity fbUser = new FbUserEntity();
+        fbUser.setLastName("Sudora");
+        fbUser.setFirstName("Simon");
+        fbUser.setGender("male");
+        fbUser.setFbId(wrongUserFbId);
+        fbUserRepository.save(fbUser);
+
+        mvc.perform(delete("/users/" + userFbId))
+                .andExpect(status().isInternalServerError());
+    }
+
+
 }
